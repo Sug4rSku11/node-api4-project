24f4cd897e51c95fbeaca492361f5b107eafcd99
Desiree Garcia <57469534+Sug4rSku11@users.noreply.github.com>
2021-12-02 17:30:27 -0700

    wip

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const path = require('path')
 const express = require('express')
 
 const server = express()
+
+const PORT = process.env.PORT || 8080
+
 server.use(express.json())
 //server.use(express.static(path.join(__dirname, 'client/build')))
 
@@ -20,7 +23,17 @@ const users = [
 server.get('/api/users', (req, res) => {
     res.json(users)
 })
-const PORT = process.env.PORT || 8080
+
+server.use('*', (req, res) => {
+    res.send(`<h1>Hello!</h1>`)
+})
+server.use((err, req, res, next) => {//eslint-disable-line 
+    res.status(500).json({
+        message: err.message,
+        stack: err.stack,
+    })
+})
+
 
 server.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
